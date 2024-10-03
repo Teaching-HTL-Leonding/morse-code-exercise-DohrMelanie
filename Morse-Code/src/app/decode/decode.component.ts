3735480730b8674819b070f238be1a8a8fbbfc1e
DohrMelanie <m.dohr@students.htl-leonding.ac.at>
2024-10-03 09:27:22 +0200

    added decode gui

--- Morse-Code/src/app/decode/decode.component.ts
+++ Morse-Code/src/app/decode/decode.component.ts
@@ -12,10 +12,29 @@ import { EncodeDecodeService } from '../encode-decode.service';
 export class DecodeComponent {
   readonly inputCode = signal("");
   readonly outputText = signal("");
+  readonly shouldDecode = signal(false);
   decodeService = inject(EncodeDecodeService);
 
   decode() {
     console.log("Decoding");
+    if (!this.isValid()) {
+      this.shouldDecode.set(false);
+      return;
+    }
+    this.shouldDecode.set(true);
     this.outputText.set(this.decodeService.decode(this.inputCode()));
+  }
+
+  isValid(): boolean {
+    if (this.inputCode() === "") {
+      return false;
+    } else {
+      for (let i = 0; i < this.inputCode().length; i++) {
+        if (this.inputCode().charAt(i) !== "." && this.inputCode().charAt(i) !== "-" && this.inputCode().charAt(i) !== " " && this.inputCode().charAt(i) !== "/") {
+          return false;
+        }
+      }
+    }
+    return true;
   }
 }
